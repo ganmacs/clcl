e268800757d5d210a7aa88dce8df27986996d5a2
ganmacs <ganmacs@gmail.com>
2015-12-04 15:57:40 +0900

    scratch

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject clcl "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [tentacles "0.3.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler clcl.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
