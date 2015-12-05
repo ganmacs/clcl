f9fbbac4578c1a1df252cb9128497509086459ae
ganmacs <ganmacs@gmail.com>
2015-12-05 13:22:12 +0900

    Add sample handler

---
+++ src/clcl/handler/sample.clj
@@ -0,0 +1,10 @@
+;; (ns clcl.handler.sample
+;; (:require [clcl.event :as event])
+;; )
+
+;; For hook, we need to implement `event-name' and `run'
+
+;; (def event-name event/issue-comment)
+
+;; (defn run [payload config]
+;;   (cool-fn payload config))
