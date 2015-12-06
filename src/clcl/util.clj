d50cfa996662c9127aa6bdb8c79644d7d3d2dd5b
ganmacs <ganmacs@gmail.com>
2015-12-06 15:40:50 +0900

    logging if env is empty or etc

--- src/clcl/util.clj
+++ src/clcl/util.clj
@@ -1,6 +1,17 @@
-(ns clcl.util)
+(ns clcl.util
+  (:require [clojure.tools.logging :as log]))
 
 (defn env [name] (System/getenv name))
 
+(defn env-with-log [n]
+  (or (env n)
+      (log/error (format "%s is not found." n))))
+
+(defn- safe-slurp [file-path]
+  (try (slurp file-path)
+       (catch java.io.FileNotFoundException e
+         (log/error (format "%s is not found." file-path)))))
+
 (defn with-oauth [config]
-  (assoc config :oauth-token (env "GITHUB_ACCESS_TOKEN")))
+  (assoc config :oauth-token
+         (env-with-log "GITHUB_ACCESS_TOKEN")))
